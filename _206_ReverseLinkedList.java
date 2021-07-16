f005ff7459a211996a3608b6e853a691afd03e49
xianhai1983 <xianhai1983@gmail.com>
2021-07-17 08:53:43 +1200

    Update _206_ReverseLinkedList.java

--- _206_ReverseLinkedList.java
+++ _206_ReverseLinkedList.java
@@ -32,4 +32,12 @@ public class _206_ReverseLinkedList {
         }
         return pre;
     }
+    
+           public ListNode reverseList(ListNode head) {
+        if (head == null || head.next== null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
 }
